a7cc01684ef08157f88401cda38011306e07d085
Vitor Rolim <victor.b.rolim@gmail.com>
2019-05-22 17:01:59 -0300

    update do codigo com as funcoes sequenciais

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics 
+import pickle
 
 class NoShowPrediction:
 
@@ -10,10 +13,20 @@ class NoShowPrediction:
 
         self.dataframe = self.preProcess(self.dataframe)
         self.dataframe.to_csv("out.csv", sep=',')
-        #...
-        #...
-        #...
+        self.train_set = self.dataframe[(self.dataframe.AppointmentDay < pd.to_datetime('2016-05-29')) & (self.dataframe.AppointmentDay >= pd.to_datetime('2016-04-29'))]
+        self.test_set = self.dataframe[self.dataframe.AppointmentDay >= pd.to_datetime('2016-05-29')]
 
+        self.model = None
+
+        self.train_set.to_csv('noshow_train.csv')
+        self.test_set.to_csv('noshow_test.csv')
+
+        self.train_set.drop(['AppointmentDay'], axis=1, inplace=True)
+        self.test_set.drop(['AppointmentDay'], axis=1, inplace=True)
+        
+        self.distColumns()
+        
+        self.createModel()
 
     #pre processamento de algumas colunas, convertendo valores, removendo colunas, etc...
     def preProcess(self, dataframe):
@@ -30,8 +43,8 @@ class NoShowPrediction:
         
         #criando novas variaveis a partir das datas
         dataframe["Day"] = dataframe["AppointmentDay"].progress_map(lambda x: x.strftime('%A'))
-        dataframe["Month"] = dataframe["AppointmentDay"].progress_map(lambda x: x.strftime('%B'))
-        dataframe["Week"] = dataframe["AppointmentDay"].progress_map(lambda x: x.strftime('%U'))
+        # dataframe["Month"] = dataframe["AppointmentDay"].progress_map(lambda x: x.strftime('%B'))
+        # dataframe["Week"] = dataframe["AppointmentDay"].progress_map(lambda x: x.strftime('%U'))
         dataframe["DistanceAppointment"] = list(map(lambda x: abs((x[0]-x[1]).days), distancesAppointment))
 
         #separando idades por grupos a cada 5 anos
@@ -49,13 +62,13 @@ class NoShowPrediction:
         dataframe['LastScheduledDay']=dataframe.groupby(['PatientId'])['ScheduledDay'].transform('max')
         dataframe['LastAppointmentDay']=dataframe.groupby(['PatientId'])['AppointmentDay'].transform('max')
         dataframe['No-show'] = dataframe['No-show'].apply(lambda x: 1 if x == 'Yes' else 0)
-        dataframe['Number_NoShow']=dataframe.groupby(['PatientId'])['No-show'].transform('sum')
-        #distancia da ultima consulta
+        # dataframe['Number_NoShow']=dataframe.groupby(['PatientId'])['No-show'].transform('sum')
+        # #distancia da ultima consulta
         lastschedule = dataframe["LastScheduledDay"]
         lastappointment = dataframe["LastAppointmentDay"]        
         lastdistanceAppointment = list(zip(lastschedule,lastappointment))
         dataframe['LastDistanceAppointment'] = list(map(lambda x: abs((x[0]-x[1]).days), lastdistanceAppointment)) 
-        #media das distancias das consultas
+        # #media das distancias das consultas
         dataframe['MeanDistanceAppointment'] = dataframe.groupby(['PatientId'])['DistanceAppointment'].transform('mean')
         # transformando do grao consulta para o grao paciente 
         #dataframe=dataframe.drop_duplicates('PatientId')
@@ -64,9 +77,65 @@ class NoShowPrediction:
         
         
         #removendo primary keys
-        dataframe.drop(["PatientId","AppointmentID"], axis=1, inplace=True)
+        dataframe.drop(["PatientId","AppointmentID","ScheduledDay","LastAppointmentDay","LastScheduledDay","Neighbourhood"], axis=1, inplace=True)
         
         return dataframe
 
+    def distColumns(self):
+        all_data = pd.concat([self.train_set,self.test_set])
+
+        for column in all_data.select_dtypes(include=[np.object]).columns:
+            #print( '\n\n', 5*'--')
+            #print('processing column: ', column)
+            all_data[column].fillna(0, inplace=True)
+
+            #print(all_data[column].value_counts())
+
+            self.train_set[column]  = self.train_set[column].fillna(0).astype(
+                pd.api.types.CategoricalDtype(categories =  all_data[column].unique()))
+            self.test_set[column]   = self.test_set[column].fillna(0).astype(
+                pd.api.types.CategoricalDtype(categories =  all_data[column].unique()))
+        print(len(self.test_set))
+        print(len(self.train_set))
+
+    def labelRemotion(self):
+        train_df = self.train_set
+        test_df = self.test_set
+
+        y_train = train_df["No-show"]
+        train_df.drop(['No-show'], axis=1, inplace=True)
+
+        y_test = test_df["No-show"]
+        test_df.drop(['No-show'], axis=1, inplace=True)
+        return train_df, y_train, test_df, y_test
+
+    def createModel(self,eval=True):
+        train_df, y_train, test_df, y_test = self.labelRemotion()
+
+        dummy_train_df = pd.get_dummies(train_df)
+        dummy_test_df = pd.get_dummies(test_df)
+
+        model = LogisticRegression()
+
+        print(10*'=', 'Training')
+        model.fit(dummy_train_df, y_train)
+
+        if(eval):
+            self.evaluation(model, dummy_test_df, y_test)
+        pickle.dump(model,open("model.pickle",'wb'))
+        return model
+
+    def evaluation(self,model, test, y_valid):
+        print(10*'=', 'evaluating on test set... ')
+        y_pred = model.predict(test)
+        print(metrics.classification_report(y_valid, y_pred))
+
+        auc = metrics.roc_auc_score(y_valid, y_pred)
+        print('\t, auc=',auc )
+
+        # Compute confusion matrix
+        print(10*'=', 'confusion matrix (test set)... ')
+        cnf_matrix = metrics.confusion_matrix(y_valid, y_pred)
+        print(cnf_matrix,"\n")
 
 instance = NoShowPrediction("./KaggleV2-May-2016.csv")
