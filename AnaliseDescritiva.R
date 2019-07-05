32b5f02740706054b9a0e1b8cd42b3172721e3f3
Rogério <rlcsf@cin.ufpe.br>
2019-07-04 23:39:32 -0300

    melhoras'

--- AnaliseDescritiva.R
+++ AnaliseDescritiva.R
@@ -19,62 +19,47 @@ db$SMS_received<-as.factor(db$SMS_received)
 #descrevendo a base
 summary(db)
 
-##Plots para as consultas
 #variavel wait
-summary(db$wait)
-describe(db$wait)
-ggplot(db, aes(x=wait,fill=Alvo)) + geom_histogram()
-ggplot(db, aes(x=Alvo,y=wait)) + geom_boxplot()
-ggplot(db, aes(x = "", y = wait)) + geom_boxplot()
+summary(mydata$wait)
+describe(mydata$wait)
+ggplot(mydata, aes(x=wait,fill=Alvo)) + geom_histogram()
+ggplot(mydata, aes(x=Age_Bin,y=wait)) + geom_boxplot()
+ggplot(mydata, aes(x = "", y = wait)) + geom_boxplot()
 
-x<- (db$wait)
-hist(x, prob=TRUE, ylim=c(0,0.2))
-curve( dchisq(x, df=5), col='green', add=TRUE)
-curve( dchisq(x, df=10), col='red', add=TRUE )
-lines( density(x), col='orange')
-
+#variavel gender
+summary(mydata$Gender)
+describe(mydata$Gender)
+ggplot(mydata, aes(x=Gender, fill=Alvo))+ geom_bar()
 
 #variavel envio de sms
-summary(db$SMS_received)
-describe(db$SMS_received)
-ggplot(db, aes(x=SMS_received, fill=Alvo))+ geom_bar()
-
-#variavel de noshow passado
-summary(db$n_noshow_passed)
-describe(db$n_noshow_passed)
-ggplot(db,aes(x=n_noshow_passed,fill=Alvo)) + geom_bar()
-ggplot(db, aes(x="",y=n_noshow_passed)) + geom_boxplot()
-
-#variavel alvo
-summary(db$Alvo)
-describe(db$Alvo)
-ggplot(db,aes(x=Alvo)) + geom_bar()
-
-#Ideia de recorrencia
-summary(db$n_noshow_passed)
-ggplot(db,aes(x=PatientId)) + geom_bar()
-
-
-#Plots para os Pacientes
-db2<- distinct(db, PatientId)
-#variavel gender
-summary(db2$Gender)
-describe(db2$Gender)
-ggplot(db2, aes(x=Gender, fill=Alvo))+ geom_bar()
-
+summary(mydata$SMS_received)
+describe(mydata$SMS_received)
+ggplot(mydata, aes(x=SMS_received, fill=Alvo))+ geom_bar()
+ggplot(mydata, aes(x=SMS_received,y=Age)) + geom_boxplot()
 
 #variavel idades
-summary(db2$Age_Bin)
-describe(db2$Age_Bin)
-ggplot(db2, aes(x=Age_Bin, fill=Alvo)) + geom_bar()
+summary(mydata$Age_Bin)
+describe(mydata$Age_Bin)
+ggplot(mydata, aes(x=Age_Bin, fill=Alvo)) + geom_bar()
 
-summary(db2$Age)
-describe(db2$Age)
-ggplot(db2, aes(x=Age, fill=Alvo)) + geom_bar()
-ggplot(db2, aes(x = Alvo, y = Age)) + geom_boxplot()
-ggplot(db2, aes(x = "", y = Age)) + geom_boxplot()
+summary(mydata$Age)
+describe(mydata$Age)
+ggplot(mydata, aes(x=Age, fill=Alvo)) + geom_histogram()
+ggplot(mydata, aes(x = Alvo, y = Age)) + geom_boxplot()
+ggplot(mydata, aes(x = Age_Bin, y = Age)) + geom_boxplot()
 
-ggplot(db2, aes(x=Age, y=Age_Bin, fill=Age_Bin)) +
+ggplot(mydata, aes(x=Age, y=Age_Bin, fill=Age_Bin)) +
   geom_density_ridges() +
   theme_ridges() + 
   theme(legend.position = "center")
+
+#variavel de noshow passado
+summary(mydata$n_noshow_passed)
+describe(mydata$n_noshow_passed)
+ggplot(mydata,aes(x=n_noshow_passed,fill=Alvo)) + geom_bar()
+ggplot(mydata, aes(x="",y=n_noshow_passed)) + geom_boxplot()
+
+#variavel no-show
+summary(mydata$Alvo)
+describe(mydata$Alvo)
+ggplot(mydata,aes(x=Alvo,fill=Alvo)) + geom_bar()
